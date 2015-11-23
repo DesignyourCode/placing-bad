ce1e01c5c25ba1f9c6d53c5dd18ad368fe565b4d
Tom Clarke <tomc@designyourcode.co.uk>
2015-11-23 13:53:17 +0000

    Adding size restriction back in.

--- public_html/index.php
+++ public_html/index.php
@@ -43,6 +43,10 @@
     });
 
     $app->get('/:width/:height/:person', function($width, $height, $person) use($app) {
+        if($width > 3000 || $height > 3000) {
+            echo "Sorry but this size is not available. Max is 3000 x 3000.";
+            die();
+        }
         serve($width, $height, $person);
     });
 
