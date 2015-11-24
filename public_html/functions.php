2b16329fe881a1ad08f8311e30e5fffe2d710d01
Simon Staton <simon.staton@live.co.uk>
2015-11-24 10:30:28 +0000

    Issue #7 - adding filter url parameters

--- public_html/functions.php
+++ public_html/functions.php
@@ -36,6 +36,74 @@ function getBestImage($width, $height, $person)
     return $dir . $match;
 }
 
+function applyFilters($img)
+{
+
+    $app = \Slim\Slim::getInstance();
+
+    // Default filter value if no parameter value given
+    $defaults = array(
+        'desaturate' => 100,
+        'blur' => 10,
+        'brightness' => 50,
+        'color' => '#FF0000',
+        'pixelate' => 8
+    );
+
+    $desaturate = $app->request()->params('desaturate');
+    if(isset($desaturate)) {
+        if(empty($desaturate)){
+            $img->desaturate($defaults['desaturate']);
+        } else {
+            $img->desaturate($desaturate);
+        }
+    }
+
+    $blur = $app->request()->params('blur');
+    if(isset($blur)) {
+        if(empty($blur)){
+            $img->blur('gaussian', $defaults['blur']);
+        } else {
+            $img->blur('gaussian', $blur);
+        }
+    }
+
+    $brightness = $app->request()->params('brightness');
+    if(isset($brightness)) {
+        if(empty($brightness)){
+            $img->brightness($defaults['brightness']);
+        } else {
+            $img->brightness($brightness);
+        }
+    }
+
+    $color = $app->request()->params('color');
+    if(isset($color)) {
+        $img->desaturate();
+        if(empty($color)){
+            $img->colorize($defaults['color'], null);
+        } else {
+            $img->colorize('#'.$color, null);
+        }
+    }
+
+    $pixelate = $app->request()->params('pixelate');
+    if(isset($pixelate)) {
+        if(empty($pixelate)){
+            $img->pixelate($defaults['pixelate']);
+        } else {
+            $img->pixelate($pixelate);
+        }
+    }
+
+    $sepia = $app->request()->params('sepia');
+    if(isset($sepia)) {
+        $img->sepia();
+    }
+    
+    return $img;
+}
+
 function serve($width, $height, $person)
 {
     $app = \Slim\Slim::getInstance();
@@ -58,6 +126,8 @@ function serve($width, $height, $person)
         $y2 = $centre + ($height / 2);
         $img->crop(0, $y1, $width, $y2);
     }
+    
+    $img = applyFilters($img);
 
     $img->output();
 }
